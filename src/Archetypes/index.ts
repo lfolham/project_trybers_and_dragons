ed4435d7e97d644c3a94f6407c084418b350da71
lfolham <lucasfolha@icloud.com>
2023-07-10 17:25:03 -0300

    task 5 - fixed index

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,10 @@
 import Archetypes from './Archetype';
 
+import Mage from './Mage';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
+import Warrior from './Warrior';
+
 export default Archetypes;
+
+export { Mage, Necromancer, Ranger, Warrior };
