f515decd56472f9400bf82def66eb0796864ea36
lfolham <lucasfolha@icloud.com>
2023-07-12 10:54:21 -0300

    task 7

--- src/Character.ts
+++ src/Character.ts
@@ -30,6 +30,72 @@ class Character implements Fighter {
       type_: this._archetype.energyType,
     }; 
   }
+
+  get dexterity(): number { 
+    return this._dexterity; 
+  }
+
+  get race(): Race { 
+    return this._race; 
+  }
+
+  get archetype(): Archetype { 
+    return this._archetype; 
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    } else {
+      this._lifePoints -= 1;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    const getRandom = getRandomInt(1, 10);
+    const maxPoints = this._maxLifePoints;
+    const sum = getRandom + maxPoints;
+
+    if (sum > this._race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    } else {
+      this._maxLifePoints = sum;
+    }
+
+    this._lifePoints = this._maxLifePoints;
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+  }
 }
 
 export default Character;
